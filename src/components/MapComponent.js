59f0d82173e048c591725572a66ae5ebf3a96839
marryday <lestnecedel@gmail.com>
2020-06-17 20:07:40 +0300

    updated map component

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,68 +1,67 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector} from "react-redux";
+import React, {useEffect, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
 import ReactDOM from "react-dom";
-import { YMaps, Map, Clusterer, Placemark } from "../ymaps";
-import points from "./points.json";
+import {YMaps, Map, Clusterer, Placemark} from "../ymaps";
+// import points from "./points.json";
+import testPoints from './testPoints.json'
 import Button from "@material-ui/core/Button";
-import { addPoint } from "../redux/actions";
+import {addPoint} from "../redux/actions";
 import {setSagaState} from '../redux/actions'
-
 const TOKEN = 'ac85ebda-7107-4441-88aa-069cf0857ea8';
-
-
 // import "../styles.css";
-
 const mapState = {
   center: [55.751574, 37.573856],
   zoom: 9,
   behaviors: ["default", "scrollZoom"],
   coordinates: []
 };
-
-const getPointData = index => {
+const getPointData = (
+  firstName,
+  lastName,
+  middleName,
+  birthDate,
+  timeOfLost,
+  clothes,
+  id) => {
+  console.log('name', firstName)
   return {
-    balloonContentBody: "acemark <strong>balloon " + index + "</strong>",
-    clusterCaption: "placemark <strong>" + index + "</strong>"
+    balloonContentBody:
+      `<address>
+      <strong>${firstName} ${lastName} ${middleName}</strong>,
+      <br/>
+      Год рождения: ${birthDate},
+      <br/>
+      Дата пропажи: ${timeOfLost},
+       <br/>
+      Комментарии: ${clothes},
+      <br/>
+      Перейти в профиль,<a href="/people/${id}"> подробнее...</a>,
+      </address>`,
+    clusterCaption: ` <strong> ${firstName} ${lastName} ${middleName}</strong>`
   };
 };
-
 const getPointOptions = () => {
   return {
     preset: "islands#violetIcon"
   };
 };
-
 export default function MapComponent() {
- 
-
+  useEffect(() => {
+    dispatch(setSagaState())
+  }, [])
   const dispatch = useDispatch();
   const [searchQuery, SetSearchQuery] = useState('')
-  const obj = {type: 'SET_STATE'}
-
-  // const useFetching = (someFetchActionCreator) => {
-  //   const dispatch = useDispatch();
-    useEffect(() => {
-      dispatch(setSagaState())
-    }, [])
-  
-  // useFetching()
-  // useEffect(() => {
-  //   dispatch(obj);
-    
-  // }, [])
-
+  const personsObjectArray = useSelector((state)=>state.mapReducer)
+  console.log(personsObjectArray)
   const handleInput = (event) => {
     SetSearchQuery(event.target.value);
   }
-
   const getInputValue = (event) => {
     event.preventDefault();
   }
-
   //poluchit' pointi
   useEffect(() => {
   }, [])
-
   return (
     <>
       <div className="AppMap">
@@ -77,23 +76,40 @@ export default function MapComponent() {
                 geoObjectHideIconOnBalloonOpen: false
               }}
             >
-              {points.map((coordinates, idx) => (
-                <Placemark
-                  key={idx}
-                  geometry={{ coordinates }}
-                  properties={getPointData(idx)}
+              {/*{points.map((coordinates, idx) => {*/}
+              {/*  console.log(coordinates)*/}
+              {/*  return <Placemark*/}
+              {/*    key={idx}*/}
+              {/*    geometry={{coordinates}}*/}
+              {/*    properties={getPointData(idx)}*/}
+              {/*    options={getPointOptions()}*/}
+              {/*  />*/}
+              {/*})}*/}
+              {testPoints.length > 0 && testPoints.map((objUser, idx) => {
+                return <Placemark
+                  key={objUser.id}
+                  geometry={{coordinates:objUser.coordinates}}
+                  properties={
+                    getPointData(
+                    objUser.firstName,
+                    objUser.lastName,
+                    objUser.middleName,
+                    objUser.birthDate,
+                    objUser.clothes,
+                    objUser.timeOfLost,
+                    objUser.id
+                    )}
                   options={getPointOptions()}
                 />
-              ))}
+              })}
             </Clusterer>
           </Map>
         </YMaps>
       </div>
       <form onSubmit={(event) => {
         event.preventDefault();
-        dispatch(addPoint(searchQuery))
-      }}>
-        <input type='text' name='inputСoordinates' onChange={handleInput} />
+        dispatch(addPoint(searchQuery))}}>
+        <input type='text' name='inputСoordinates' onChange={handleInput}/>
         <Button variant="contained" color="primary" type='submit'>
           Добавить точку
         </Button>
@@ -102,6 +118,3 @@ export default function MapComponent() {
     </>
   );
 }
-
-
-
