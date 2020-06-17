759bf85c14749a35a56d165fe00f20ad1259febb
marryday <lestnecedel@gmail.com>
2020-06-17 21:30:57 +0300

    skelet for counting

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -6,7 +6,6 @@ const User = require("../models/userSchema");
 const fs = require("fs");
 
 router.post("/", (req, res) => {
-
   if (req.files === null) {
     return res.status(400).json({ msg: "No file was uploaded" });
   }
@@ -19,21 +18,20 @@ router.post("/", (req, res) => {
     res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
   });
 });
-const getAges = (year) =>{ 
-  if(year)
-  return (new Date().getFullYear() - Number(year))
-}
+const getAges = (year) => {
+  if (year) return new Date().getFullYear() - Number(year);
+};
 
 router.post("/missedperson", async (req, res) => {
-  console.log('ya tut')
+  console.log("ya tut");
   try {
     const fio = req.body.name.split(" ");
     const firstName = fio[1];
     const middleName = fio[2];
     const lastName = fio[0];
-   const ages = getAges(String(req.body.birthday).slice(0,4))
+    const ages = getAges(String(req.body.birthday).slice(0, 4));
     const author = await User.findById(req.body.author);
-    console.log(req.body.url)
+    console.log(req.body.url);
     const poteryash = await new Poteryash({
       firstName,
       lastName,
@@ -64,8 +62,8 @@ router.post("/missedperson", async (req, res) => {
 
 router.get("/missedpeople", async (req, res) => {
   try {
-    let ppl = await Poteryash.find()
-    console.log(ppl)
+    let ppl = await Poteryash.find();
+    console.log(ppl);
     res.json({ ppl });
   } catch (e) {
     res.status(500).json(e);
@@ -82,6 +80,21 @@ router.patch("/coordinates", async (req, res) => {
   } catch (e) {
     console.error(e.message);
     res.json(e);
+  }
+});
+
+router.get("/countcoordinates", async (req, res) => {
+  try {
+    let user = await User.findById(req.body.user);
+    let poteryashes = await Poteryash.find();
+    let matches = poteryashes.filter(
+      poteryashes.map((p) => p.coordinates[0] + p.coordinates[1]) -
+        user.coordinates[0] +
+        user.coordinates[1] < 0.5
+    );
+    console.log(matches);
+  } catch (e) {
+    res.json(e)
   }
 });
 module.exports = router;
